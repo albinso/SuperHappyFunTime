ae31ab960e0a2b2aa9e03f5405816f9eccb59f40
MisterWeasel <heerkip@gmail.com>
2014-08-09 19:29:48 +0200

    Players take no fall damage.

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -2,3 +2,21 @@ AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 
 include("shared.lua")
+
+function GM:PlayerSpawn(ply)
+	self.BaseClass:PlayerSpawn( ply )   
+ 
+    ply:SetGravity  ( 3 )  -- Although I suggest 1, becuase .75 is just too low.
+    ply:SetMaxHealth( 100, true )  
+ 
+    ply:SetWalkSpeed( 325 )  
+    ply:SetRunSpeed ( 1000 )
+    ply:SetJumpPower(500000)
+end
+
+function GM:EntityTakeDamage(target, damageinfo)
+	if target:IsPlayer() and damageinfo:IsFallDamage() then
+		damageinfo:SetDamage(0)
+	end
+	return damageinfo
+end
